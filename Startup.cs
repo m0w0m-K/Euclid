e2684d7a3d190a0905f5219129d1fcec0eae583c
-K <144549607+m0w0m-K@users.noreply.github.com>
2026-08-20 15:39:01 +0900

    Install all-effect marker setting

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ namespace Euclid
         internal static void Load(UnityModManager.ModEntry modEntry)
         {
             EuclidMod.Load(modEntry);
+            AllEffectMarkerSettings.Install(modEntry);
             PositionTrackFocusSync.Install();
             AllEffectMarkerOverlay.Install();
         }
